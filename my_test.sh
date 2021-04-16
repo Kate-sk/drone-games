401c10999749279ff811ea711ddb7e7ef86eaa5d
Kate-sk <Ekaterina.Karmanova@skoltech.ru>
2021-04-16 21:36:49 +0300

    test

---
+++ my_test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+num=6
+model="iris"
+world="formation.world"
+
+if [ "$1" == "prof" ]; then
+  arg="--gazebo_ros"
+elif [ "$1" == "nonprof" ]; then
+  arg=""
+else
+  echo "$0 (prof | nonprof) [mc | vtol] [num] [world]"
+  exit
+fi
+
+if [ "$2" == "vtol" ]; then
+  model="standard_vtol"
+fi
+
+if [ "$3" != "" ]; then
+  num=$3
+fi
+
+if [ "$4" != "" ]; then
+  world=$4
+fi
+
+./start.sh $model $num gazebo/worlds/$world --ref_point 0,-72,0.1 $arg $@
+
+if [ "$1" == "prof" ]; then
+  ./bin/formations_gen.py $model $num formation/borders.txt formation/test_fs/ --names T E C T &
+fi
+
+echo "waiting ..."
+sleep 3
+
+./examples/my_form_test.py $mode
+
+read -p "Press enter to stop ..."
+
+./stop.sh
